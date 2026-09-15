85021e7ebaa25382b48f55a00b5ac7633ca0649c
SAO AUTO TRACTOR <kallusao222@gmail.com>
2026-09-15 19:33:18 +0530

    Update app

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-caf3a6a6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.njma43rf65o"
+    "revision": "0.7fllqk1r2j8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
